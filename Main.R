222a106904e6d625ebbd83f7a413b5110f948ee4
w1ndstr5ck <84208375+w1ndstr5ck@users.noreply.github.com>
2022-12-04 21:54:55 -0600

    Add files via upload

--- Main.R
+++ Main.R
@@ -29,8 +29,20 @@ head(movies_df)
 #count the genres list for each movie we we know the count of the genres
 movies_df <- movies_df %>% rowwise() %>% mutate(genres.count = length(genres))
 
-#only keep the rating and genres.count columns so we can do anaysis on them
-movies_df <- movies_df[4:5]
-head(movies_df)
+#only keep the rating and genres.count columns also group by and summarize ratings by number of genres so we can do analysis on them
+anaysis_df <- movies_df %>% group_by(genres.count) %>% summarise(rating.count = mean(rating, na.rm = TRUE))
+anaysis_df
 
-
+library(car)
+scatterplot(anaysis_df$genres.count, anaysis_df$rating.count)
+model_linear <- lm(data = anaysis_df, formula = genres.count ~ rating.count)
+model_poly_2 <- lm(data = anaysis_df, formula = genres.count ~ poly(rating.count, 2))
+model_poly_3 <- lm(data = anaysis_df, formula = genres.count ~ poly(rating.count, 3))
+names(r.squared) <- c(1:3)
+r.squared <- c("model_linear", "model_poly_2", "model_poly_3")
+r.squared["model_linear"] <- summary(model_linear)$r.squared
+r.squared["model_poly_2"] <- summary(model_poly_2)$r.squared
+r.squared["model_poly_3"] <- summary(model_poly_3)$r.squared
+lowest_corr <- max(r.squared)
+library(GGally)
+ggpairs(anaysis_df)
